f1f9f1c270c28ff7013aab2e6b3b48caba5522ef
David Echeli <davidecheli@gmail.com>
2018-04-02 10:45:51 -0300

    Adding firestore and firebase to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ConfigService } from './services/config.service';
@@ -9,6 +14,8 @@ import { TimerListComponent } from './timer-list/timer-list.component';
 import { TimerItemComponent } from './timer-list/timer-item/timer-item.component';
 import { TimerListActionsComponent } from './timer-list/timer-list-actions/timer-list-actions.component';
 import { TimerService } from './services/timer.service';
+
+import { credentials } from './config';
 
 
 @NgModule({
@@ -21,7 +28,11 @@ import { TimerService } from './services/timer.service';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AngularFireModule.initializeApp(credentials.firebase),
+    AngularFirestoreModule.enablePersistence(),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule
   ],
   providers: [ConfigService, TimerService],
   bootstrap: [AppComponent]
